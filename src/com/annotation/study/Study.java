d2b6b4f2b6255bd13989aded9cf5e8e3858e18f0
machao521py <machao521hw@163.com>
2017-02-17 20:28:00 +0800

    Initial commit

---
+++ src/com/annotation/study/Study.java
@@ -0,0 +1,11 @@
+package com.annotation.study;
+
+/**
+ * Created by machao on 2016/10/19.
+ */
+public class Study {
+
+    public static void main(String[] args){
+        System.out.println("nihao");
+    }
+}
